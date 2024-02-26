679bde9f9782817ce387a8b9d7209a72f3eb221f
Raj N <nmrajanikanth@gmail.com>
2024-02-25 21:40:15 -0500

    Updating for @autowired

--- src/main/java/com/service/mrkstore/controller/BookController.java
+++ src/main/java/com/service/mrkstore/controller/BookController.java
@@ -1,7 +1,7 @@
 package com.service.mrkstore.controller;
 import com.service.mrkstore.model.Book;
 import com.service.mrkstore.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,9 +9,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-
+    private final BookRepository bookRepository;
     @Autowired
-    private BookRepository bookRepository;
+    BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping
     public List<Book> getAllBooks() {
